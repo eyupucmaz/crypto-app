20e6fc12a4b6e0d179394d2e4992a14ba88d69e5
Eyup Ucmaz <eyupucmaz@gmail.com>
2022-02-21 15:11:53 +0300

    ♻️ remove states, add cstm hook, filtering improve

--- src/App.js
+++ src/App.js
@@ -1,57 +1,30 @@
-import { useState, useEffect } from "react";
 import styles from "./App.module.css";
 import Coin from "./components/Coin/Coin";
 import Header from "./components/Header/Header";
-import { CoinContext } from "./Context/CoinContext";
-import axios from "axios";
+import { useCoin } from "./Context/CoinContext";
 import { VscLoading } from "react-icons/vsc";
+import { useEffect } from "react";
 
-const API_URL =
-	"https://api.coinstats.app/public/v1/coins?skip=0&limit=1000&currency=";
 function App() {
-	const [searchInput, setSearchInput] = useState("");
-	const [coinData, setCoinData] = useState([]);
-	const [currency, setCurrency] = useState("USD");
-	const [error, setError] = useState(null);
-	const [loading, setLoading] = useState(true);
-
-	useEffect(() => {
-		axios
-			.get(`${API_URL}${currency}`)
-			.then((res) => {
-				setCoinData(res.data.coins);
-			})
-			.catch((err) => {
-				setError(err.message);
-			})
-			.finally(() => {
-				setLoading(false);
-			});
-	}, [currency]);
-
-	const filteredList = coinData.filter((coin) => {
-		return coin.name.toLowerCase().includes(searchInput.toLowerCase());
-	});
+	const { error, loading, filteredList } = useCoin();
+	// Keep updating while filteredList has been changed
+	useEffect(() => {}, [filteredList]);
 
 	return (
 		<div className={styles.container}>
-			<CoinContext.Provider
-				value={{ searchInput, setSearchInput, coinData, setCurrency, currency }}
-			>
-				<Header />
-
-				{error && `${error}`}
-				{loading && (
-					<span className={styles.loading}>
-						<VscLoading />
-					</span>
-				)}
-				<section className={styles.coins}>
-					{filteredList.map((coin) => {
+			<Header />
+			{error && `${error}`}
+			{loading && (
+				<span className={styles.loading}>
+					<VscLoading />
+				</span>
+			)}
+			<section className={styles.coins}>
+				{!loading &&
+					filteredList?.current?.map((coin) => {
 						return <Coin key={coin.rank} data={coin} />;
 					})}
-				</section>
-			</CoinContext.Provider>
+			</section>
 		</div>
 	);
 }
